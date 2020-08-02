70e48b131d69fecffa605b5901e31cd0cc9be805
Paolo Z Roxas <paolo.roxas@gusto.com>
2020-08-01 17:39:34 -0700

    ex2 complete

--- machine-learning-ex2/ex2/costFunctionReg.m
+++ machine-learning-ex2/ex2/costFunctionReg.m
@@ -17,10 +17,14 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
+[nonRegularizedJ, nonRegularizedGrad] = costFunction(theta, X, y);
 
+% Cost
+J = nonRegularizedJ + (lambda/(2*m)) * sum(theta(2:end) .^ 2);
 
-
-
+% Gradient
+restOfGrad = nonRegularizedGrad(2:end) + (lambda/m) .* theta(2:end);
+grad = [nonRegularizedGrad(1); restOfGrad];
 
 % =============================================================
 
